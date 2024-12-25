670f79a1b81b5452135ceacb3b1d8b6bcee55dac
Wafeechan <awn.1905@gmail.com>
2024-12-25 16:11:14 +0800

    Update dependancies

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,35 @@
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'
 
 function navbar(){
 
 return(
-    <>
-    
-    </>
+    <body className='w-full text-white font-serif'>
+            <div className='m-0'> 
+                
+                <nav className='fixed top-0 left-0 z-10 h-max w-full items-center rounded-none px-4 py-2 md:py-2 lg:px-8 lg:py-4 bg-gray-50/30 backdrop-blur-md backdrop-brightness-10 flex justify-between'>
+                    <div className='flex items-center logo-container'>
+                        <div className="logo"></div>
+                    </div>
+                    <div className="flex items-center justify-end text-blue-gray-900">
+                        <ul className='flex space-x-4 my-2 lg:my-4'>
+                            <li className=' navBtn'>
+                                <Link to="/" className='nav-link navHover text-3xl'>Home</Link>
+                            </li>
+                            <li className='navBtn'>
+                                <Link to="/portfolio" className='nav-link navHover text-3xl'>Portfolio</Link>
+                            </li >
+                            <li className='navBtn'>
+                                <Link to="/about" className='nav-link navHover text-3xl'>About</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                
+            </div>
+        </body>
+        
 )
 
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import Home from './Home.jsx';
+//import Portfolio from './Portfolio.jsx';
+//import About from './About.jsx';
+import Navbar from '/src/components/navbar.jsx';
+import './index.css';
 
+
+
+function Main() {
+  return (
+  <Router basename="/wafi-portfolio">
+    <Navbar />
+
+    {/* <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/about" element={<About />} />
+    </Routes> */}
+  </Router>
+  )
+}
+
+// Render the application into the DOM
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
