435cbd658b84ae0faec3de71e5e203fa244c99d3
Wafeechan <awn.1905@gmail.com>
2024-12-25 17:19:52 +0800

    Reset navbar & css stuffs

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from "./components/navbar";
 function App() {
   return (
     <div>
-      <Navbar />
       <div className="mt-16 p-4">
         <h1 className="text-3xl font-bold">Welcome to the website!</h1>
       </div>
