338cf08a3a8f6ccc00a820ffe3236add80d327d8
Wafeechan <awn.1905@gmail.com>
2024-12-25 17:31:09 +0800

    Trying to fix tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './webpages/Home.jsx';
-import Portfolio from './webpages/Portfolio.jsx';
-import About from './webpages/About.jsx';
-import Navbar from './components/navbar.jsx';
+// import Home from './webpages/Home.jsx';
+// import Portfolio from './webpages/Portfolio.jsx';
+// import About from './webpages/About.jsx';
+// import Navbar from './components/navbar.jsx';
 import './index.css';
 
 
 
 function Main() {
   return (
-  <Router basename='web-portfolio'>
-    <Navbar />
+  // <Router basename='web-portfolio'>
+  //   <Navbar />
     
-    <div className='bg-blue-500 text-white p-4'>Tailwind is working</div>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </Router>
+  //   <div className='bg-blue-500 text-white p-4'>Tailwind is working</div>
+  //   <Routes>
+  //     <Route path="/" element={<Home />} />
+  //     <Route path="/portfolio" element={<Portfolio />} />
+  //     <Route path="/about" element={<About />} />
+  //   </Routes>
+  // </Router>
   
+  <div className='bg-blue-500 text-white p-4'>Tailwind is working</div>
   )
 }
 
