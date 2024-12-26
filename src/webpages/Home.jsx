826f08a492953715ab75aa095d75c059af308aae
Wafeechan <awn.1905@gmail.com>
2024-12-26 16:54:51 +0800

    Added components.css

--- src/webpages/Home.jsx
+++ src/webpages/Home.jsx
@@ -1,4 +1,5 @@
 import AW from '/assets/AW_Image.png'
+import "./components.css"
 
 function Home(){
 
@@ -7,11 +8,14 @@ function Home(){
         <body className="text-black bg-blue-400">
             <div className="grid grid-cols-[4fr,5fr] gap-4 h-screen relative">
                 <div className="relative h-full">
-                    <h1 className="text-center">Test 1</h1>
                     <img src={AW} alt="" className='absolute bottom-0 mb-[70px] left-0 ml-[50px] w-full max-h-[600px] object-contain'/>
                 </div>
                 <div className="">
-                    <h1>Test 2</h1>
+                    <div className="bg-white w-full h-full max-w-[600px] max-h-[400px] mx-auto my-[200px] rounded-2xl text-center">
+                        <p className="home-text">Hello, I am</p>
+                        <p>Abdul Wafi,</p>
+                        <p>a programmer.</p>
+                    </div>
                 </div>
 
             </div>
