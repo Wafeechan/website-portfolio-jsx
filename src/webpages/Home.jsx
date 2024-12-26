80dd8409fea9b6a07f4da551b999ea5f2a5c89ed
Wafeechan <awn.1905@gmail.com>
2024-12-26 16:46:49 +0800

    Update Home.jsx

--- src/webpages/Home.jsx
+++ src/webpages/Home.jsx
@@ -1,14 +1,16 @@
+import AW from '/assets/AW_Image.png'
 
 function Home(){
 
     return(
         <>
-        <body className="text-black mt-32">
-            <div className="grid grid-cols-[4fr,5fr] gap-4 h-screen">
-                <div className="bg-blue-500">
+        <body className="text-black bg-blue-400">
+            <div className="grid grid-cols-[4fr,5fr] gap-4 h-screen relative">
+                <div className="relative h-full">
                     <h1 className="text-center">Test 1</h1>
+                    <img src={AW} alt="" className='absolute bottom-0 mb-[70px] left-0 ml-[50px] w-full max-h-[600px] object-contain'/>
                 </div>
-                <div className="bg-blue-900">
+                <div className="">
                     <h1>Test 2</h1>
                 </div>
 
