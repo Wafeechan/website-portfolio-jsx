3202d5e1d4bcc2c2e0a7e9516c77b935ef209093
Wafeechan <awn.1905@gmail.com>
2024-12-29 00:24:57 +0800

    Update About.jsx

--- src/webpages/About.jsx
+++ src/webpages/About.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import AboutInfo from './About-info.jsx';
 
 import ID from '/assets/customID.png';
 import LogoBlue from '/assets/logo_blue2.png';
@@ -8,23 +7,20 @@ function About(){
 
     return(
         <>
-        <Routes>
-            <Route path="/about-info" element={<AboutInfo />} />
-        </Routes>
-        
-        <body className='bg-blue-400 h-screen pb-[100px]'>
+
+        <div className='bg-blue-400 h-screen pb-[100px]'>
             <div className='grid md:grid-rows-2 grid-rows-[1fr, 2fr] h-full'>
                 <div className='md:w-[400px] w-[350px] h-auto m-auto md:pt-[140px] pt-[80px]'>
                     <img src={ID} alt="" className='rounded-lg' />
                 </div>
 
                 <div className='grid md:grid-cols-4 md:grid-rows-none grid-cols-2 grid-rows-2'>
-                    <div className='relative book-button book-left grid grid-rows-2 book-hover group'>
+                    <Link to='/about-info' className='relative book-button book-left grid grid-rows-2 book-hover group'>
                         <div className='top-half-book'></div>
                         <div className='absolute translate logo-book z-10'><img src={LogoBlue} alt="" /></div>
                         <div></div>
                         <div className='book-text book-group-hover'>Education</div>
-                    </div>
+                    </Link>
                     <div className='relative book-button book-right grid grid-rows-2 book-hover group'>
                         <div className='top-half-book'></div>
                         <div className='absolute translate logo-book z-10'><img src={LogoBlue} alt="" /></div>
@@ -46,7 +42,7 @@ function About(){
                 </div>
 
             </div>
-        </body>
+        </div>
         </>
     )
 }
