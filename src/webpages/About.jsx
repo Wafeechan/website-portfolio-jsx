fdaffd29e054d3fa6b752c691d63d522ad0ec548
Wafeechan <awn.1905@gmail.com>
2024-12-29 00:15:40 +0800

    About.jsx updated

--- src/webpages/About.jsx
+++ src/webpages/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import ID from '/assets/customID.png';
+import LogoBlue from '/assets/logo_blue2.png';
 
 function About(){
 
     return(
         <>
+
         <body className='bg-blue-400 h-screen pb-[100px]'>
             <div className='grid md:grid-rows-2 grid-rows-[1fr, 2fr] h-full'>
                 <div className='md:w-[400px] w-[350px] h-auto m-auto md:pt-[140px] pt-[80px]'>
@@ -15,21 +17,25 @@ function About(){
                 <div className='grid md:grid-cols-4 md:grid-rows-none grid-cols-2 grid-rows-2'>
                     <div className='relative book-button book-left grid grid-rows-2 book-hover group'>
                         <div className='top-half-book'></div>
+                        <div className='absolute translate logo-book z-10'><img src={LogoBlue} alt="" /></div>
                         <div></div>
                         <div className='book-text book-group-hover'>Education</div>
                     </div>
                     <div className='relative book-button book-right grid grid-rows-2 book-hover group'>
                         <div className='top-half-book'></div>
+                        <div className='absolute translate logo-book z-10'><img src={LogoBlue} alt="" /></div>
                         <div></div>
                         <div className='book-text book-group-hover'>Experience</div>
                     </div>
                     <div className='relative book-button book-left grid grid-rows-2 book-hover group'>
                         <div className='top-half-book'></div>
+                        <div className='absolute translate logo-book z-10'><img src={LogoBlue} alt="" /></div>
                         <div></div>
                         <div className='book-text book-group-hover'>Achievement</div>
                     </div>
                     <div className='relative book-button book-right grid grid-rows-2 book-hover group'>
                         <div className='top-half-book'></div>
+                        <div className='absolute translate logo-book z-10'><img src={LogoBlue} alt="" /></div>
                         <div></div>
                         <div className='book-text book-group-hover'>Skills</div>
                     </div>
