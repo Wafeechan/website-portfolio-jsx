e519dbc0cb8129a7f4a8e8ebf795e3d4093a16e1
Wafeechan <awn.1905@gmail.com>
2024-12-28 23:17:31 +0800

    About page book layout

--- src/webpages/About.jsx
+++ src/webpages/About.jsx
@@ -6,17 +6,17 @@ function About(){
 
     return(
         <>
-        <body className='bg-blue-400 h-screen'>
-            <div className='grid grid-rows-2 h-full'>
-                <div className='w-[400px] h-auto m-auto pt-[70px]'>
+        <body className='bg-blue-400 h-screen pb-[100px]'>
+            <div className='grid md:grid-rows-2 grid-rows-[1fr, 2fr] h-full'>
+                <div className='md:w-[400px] w-[350px] h-auto m-auto md:pt-[70px] pt-[80px]'>
                     <img src={ID} alt="" className='rounded-lg' />
                 </div>
 
-                <div className='grid grid-cols-4'>
-                    <div className='book-button'></div>
-                    <div className='book-button'></div>
-                    <div className='book-button'></div>
-                    <div className='book-button'></div>
+                <div className='grid md:grid-cols-4 md:grid-rows-none grid-cols-2 grid-rows-2'>
+                    <div className='book-button book-left'></div>
+                    <div className='book-button book-right'></div>
+                    <div className='book-button book-left'></div>
+                    <div className='book-button book-right'></div>
                 </div>
 
             </div>
