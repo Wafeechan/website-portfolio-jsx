2098a486a570a29680b8eeade11dc533f1a695fc
Wafeechan <awn.1905@gmail.com>
2024-12-28 22:42:06 +0800

    Update modal.jsx

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -5,7 +5,7 @@ function Modal({ isOpen, onClose, image, title, text }) {
 
     return(
         <div className='fixed inset-0 bg-gray-900 bg-opacity-60 flex justify-center z-50'>
-            <div className='bg-white w-[830px] h-[500px] p-6 relative shadow-lg mt-[50px] z-50'>
+            <div className='bg-white md:w-[830px] md:h-[500px] w-[500px] h-[280px] p-6 relative shadow-lg mt-[50px] z-50'>
                 <button
                     onClick={onClose}
                     className='absolute top-3 right-3 text-gray-600 hover:text-gray-800 text-2xl font-bold'
@@ -13,8 +13,8 @@ function Modal({ isOpen, onClose, image, title, text }) {
 
                 {/* Modal content */}
                 <img src={image} alt="Modal Content" className='w-full h-full object-cover shadow-sm' />
-                <h2 className='text-center text-white text-4xl font-Inter font-semibold mt-10'>{title}</h2>
-                <p className='text-center mt-4 text-white font-Inter font-light'>{text}</p>
+                <h2 className='text-center text-white md:text-4xl text-3xl font-Inter font-semibold mt-10'>{title}</h2>
+                <p className='text-justify mt-4 text-white font-Inter font-light md:text-base text-sm'>{text}</p>
             </div>
         </div>
 
