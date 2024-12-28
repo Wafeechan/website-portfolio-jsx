6c154121c0b5e530f91150d81ff7c8317d64311c
Wafeechan <awn.1905@gmail.com>
2024-12-28 23:02:19 +0800

    About.jsx layout

--- src/webpages/About.jsx
+++ src/webpages/About.jsx
@@ -1,8 +1,26 @@
+import React from 'react';
+
+import ID from '/assets/customID.png';
 
 function About(){
 
     return(
         <>
+        <body className='bg-blue-400 h-screen'>
+            <div className='grid grid-rows-2 h-full'>
+                <div className='w-[400px] h-auto m-auto pt-[70px]'>
+                    <img src={ID} alt="" className='rounded-lg' />
+                </div>
+
+                <div className='grid grid-cols-4'>
+                    <div className='book-button'></div>
+                    <div className='book-button'></div>
+                    <div className='book-button'></div>
+                    <div className='book-button'></div>
+                </div>
+
+            </div>
+        </body>
         </>
     )
 }
