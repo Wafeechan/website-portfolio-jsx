82da5bc0c312f7ca4350c7456cf7865c52639afd
Wafeechan <awn.1905@gmail.com>
2024-12-28 17:44:15 +0800

    Update Portfolio.jsx
    Added Modal

--- src/webpages/Portfolio.jsx
+++ src/webpages/Portfolio.jsx
@@ -1,18 +1,27 @@
 import React, {useState} from 'react';
 import ImageSlider from '/src/components/imageSlider.jsx';
+import Modal from '/src/components/modal.jsx'
 
 import Image1 from '/src/assets/image/placeholder1.jpg';
 import Image2 from '/src/assets/image/placeholder2.jpg';
 import Image3 from '/src/assets/image/placeholder3.png';
 
 function Portfolio() {
-  const images = [
-    Image1,
-    Image2,
-    Image3,
+  const images = [Image1, Image2, Image3];
+  const titles = ['Portfolio 1', 'Portfolio 2', 'Portfolio 3'];
+  const texts = [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
+
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
+
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
   ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsOpenModal(true);
+  const closeModal = () => setIsModalOpen(false);
 
   return (
     <body className="bg-blue-400 h-screen">
@@ -30,7 +39,12 @@ function Portfolio() {
           
             {/* Image Slider */}
             <div className="relative mt-[10px] md:w-[800px] md:h-[450px] w-[500px] h-[280px] object-contain z-10">
-                <div className='absolute rounded-full w-[25px] h-[25px] bg-white z-30 text-xl shadow-md flex items-center justify-center top-[15px] right-[30px]'>+</div>
+                {/* Modal trigger */}
+                <div 
+                  onClick={openModal} 
+                  className='absolute rounded-full w-[25px] h-[25px] bg-white z-30 text-xl shadow-md flex items-center justify-center top-[15px] right-[30px]'
+                >+</div>
+
                 <ImageSlider images={images} width="100%" height="100%" onSlideChange={(index) => setCurrentImageIndex(index)}/>
                 <div className='absolute md:w-[150px] md:h-[40px] w-[100px] h-[30px] text-center z-30 bg-teal-400 right-[30px] bottom-[-20px] shadow-lg flex items-center justify-center md:text-md text-sm'>{`${currentImageIndex + 1}/${images.length}`}</div>
             </div>
@@ -38,6 +52,14 @@ function Portfolio() {
           
         </div>
       </div>
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        image={images[currentImageIndex]}
+        title={titles[currentImageIndex]}
+        text={texts[currentImageIndex]}
+      />
     </body>
   );
 }
