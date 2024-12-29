5698a40196234f5f2b1c43704f2568c39f2ed8a7
Wafeechan <awn.1905@gmail.com>
2024-12-29 16:46:34 +0800

    Adjusted for phone layout

--- src/components/bottombar.jsx
+++ src/components/bottombar.jsx
@@ -2,8 +2,8 @@ function Bottombar(){
     return(
         <body className='w-full text-gray-700 font-Inter-thin font-thin z-50'>
             <div className="m-0">
-                <nav className="fixed bottom-0 left-0 z-10 w-full  bg-white shadow-md">
-                    <div className="cotainer mx-auto justify-between text-center my-6">
+                <nav className="fixed bottom-0 left-0 z-10 w-full h-[50px] sm:h-[70px] bg-white shadow-md">
+                    <div className="cotainer mx-auto sm:my-6 my-3 justify-between text-center">
                         <h1>Created with Vite, React & Tailwind CSS</h1>
                     </div>
                 </nav>
