67b6b1422bf22eaec8675de040a8f13749200b28
Wafeechan <awn.1905@gmail.com>
2024-12-29 15:57:59 +0800

    Update Portfolio.jsx

--- src/webpages/Portfolio.jsx
+++ src/webpages/Portfolio.jsx
@@ -27,18 +27,18 @@ function Portfolio() {
     <div className="bg-blue-400 h-[120vh] w-screen-lg pt-[150px]">
       <div className=" items-center relative">
         {/* Title */}
-        <div className="font-Inter text-4xl md:text-5xl text-center mb-[20px] text-white font-bold">Portfolio</div>
+        <div className="font-Inter text-3xl sm:text-4xl md:text-5xl text-center mb-[20px] text-white font-bold">Portfolio</div>
 
-        <div className=' mx-auto bg-blue-600 md:w-[850px] md:h-[30px] w-[520px] h-[20px] z-20 relative'/>  
+        <div className=' mx-auto bg-blue-600 md:w-[850px] md:h-[30px] sm:w-[520px] sm:h-[20px] w-[300px] h-[15px] z-20 relative'/>  
 
         {/* Image Slider with Background */}
         <div className="relative w-full flex justify-center pb-[200px]">
 
             {/* Rectangle Background */}
-            <div className="absolute top-[0px] bg-white md:w-[830px] md:h-[500px] w-[510px] h-[320px] shadow-lg z-0"></div>
+            <div className="absolute top-[0px] bg-white md:w-[830px] md:h-[500px] sm:w-[510px] sm:h-[320px] w-[290px] h-[200px] shadow-lg z-0"></div>
           
             {/* Image Slider */}
-            <div className="relative mt-[10px] md:w-[800px] md:h-[450px] w-[500px] h-[280px] object-contain z-10">
+            <div className="relative sm:mt-[10px] mt-[5px] md:w-[800px] md:h-[450px] sm:w-[500px] sm:h-[280px] w-[270px] h-[190px] object-contain z-10">
                 {/* Modal trigger */}
                 <button 
                   onClick={openModal} 
@@ -46,7 +46,7 @@ function Portfolio() {
                 >+</button>
 
                 <ImageSlider images={images} width="100%" height="100%" onSlideChange={(index) => setCurrentImageIndex(index)}/>
-                <div className='absolute md:w-[150px] md:h-[40px] w-[100px] h-[30px] text-center z-30 bg-cyan-300 right-[30px] bottom-[-20px] shadow-lg flex items-center justify-center md:text-md text-sm'>{`${currentImageIndex + 1}/${images.length}`}</div>
+                <div className='absolute md:w-[150px] md:h-[40px] sm:w-[100px] sm:h-[30px] w-[75px] h-[25px] text-center z-30 bg-cyan-300 right-[30px] bottom-[-20px] shadow-lg flex items-center justify-center md:text-md sm:text-sm text-xs'>{`${currentImageIndex + 1}/${images.length}`}</div>
             </div>
 
           
