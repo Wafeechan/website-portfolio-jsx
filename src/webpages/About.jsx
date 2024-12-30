36b47f4b2a8048fa5944af4ebe15bdeb9aaa75b1
Wafeechan <awn.1905@gmail.com>
2024-12-30 15:26:53 +0800

    UI changes

--- src/webpages/About.jsx
+++ src/webpages/About.jsx
@@ -9,43 +9,78 @@ function About(){
     return(
         <>
 
-        <div className='bg-blue-400 h-screen pb-[100px]'>
-            <div className='grid md:grid-rows-2 grid-rows-[1fr, 2fr] gap-4 h-full'>
-                <div className='md:w-[400px] w-[350px] h-auto m-auto md:pt-[140px] pt-[80px]'>
-                    <img src={ID} alt="" className='rounded-lg' />
+        <div className='bg-blue-400 h-full md:pb-[50px] pb-[300px]'>
+            <div className='grid md:grid-rows-[4fr, 3fr] grid-rows-[1fr, 2fr] gap-4 h-screen'>
+                <div className='flex justify-center items-center h-full mt-[120px]'>
+                    <div className='w-auto h-full max-w-full max-h-full'>
+                        <img src={ID} alt="" className='rounded-lg md:w-[500px] w-[300px] h-auto object-contain' />
+                    </div>
+                    
                 </div>
 
-                <div className='grid md:grid-cols-4 md:grid-rows-none grid-cols-2 grid-rows-2'>
-                    <div className='px-[100px]'>
-                        <Link to='/about-info' className='relative book-button book-left grid grid-rows-2 book-hover group'>
-                            <div className='top-half-book'></div>
-                            <div className='absolute translate logo-book z-10'><img src={LogoBlue} alt="" /></div>
+                <div className='grid md:grid-cols-4 md:grid-rows-none grid-cols-2 grid-rows-2 md:gap-[100px] gap-[50px]'>
+                    <div className='book-space'>
+                        <Link to='/about-info' className='relative book-button book-left grid grid-rows-[3fr,1fr] book-hover group'>
+                            {/* Centered White Background */}
+                            <div className='top-half-book flex items-center justify-center'>
+                            <img src={LogoBlue} alt="" className='logo-book absolute' />
+                            </div>
+
+                            {/* Spacer Div for Proper Positioning */}
                             <div></div>
-                            <div className='book-text book-group-hover'>Education</div>
+
+                            {/* Text Positioned at 3/4th */}
+                            <div className='book-text book-group-hover flex items-end justify-center'>
+                                Education
+                            </div>
                         </Link>
                     </div>
-                    <div className='px-[100px]'>
-                        <Link to='/about-info' className='relative book-button book-left grid grid-rows-2 book-hover group'>
-                            <div className='top-half-book'></div>
-                            <div className='absolute translate logo-book z-10'><img src={LogoBlue} alt="" /></div>
+                    <div className='book-space'>
+                        <Link to='/about-info' className='relative book-button book-left grid grid-rows-[3fr,1fr] book-hover group'>
+                            {/* Centered White Background */}
+                            <div className='top-half-book flex items-center justify-center'>
+                            <img src={LogoBlue} alt="" className='logo-book absolute' />
+                            </div>
+
+                            {/* Spacer Div for Proper Positioning */}
                             <div></div>
-                            <div className='book-text book-group-hover'>Experience</div>
+
+                            {/* Text Positioned at 3/4th */}
+                            <div className='book-text book-group-hover flex items-end justify-center'>
+                                Experience
+                            </div>
                         </Link>
                     </div>
-                    <div className='px-[100px]'>
-                        <Link to='/about-info' className='relative book-button book-left grid grid-rows-2 book-hover group'>
-                            <div className='top-half-book'></div>
-                            <div className='absolute translate logo-book z-10'><img src={LogoBlue} alt="" /></div>
+                    <div className='book-space'>
+                        <Link to='/about-info' className='relative book-button book-left grid grid-rows-[3fr,1fr] book-hover group'>
+                            {/* Centered White Background */}
+                            <div className='top-half-book flex items-center justify-center'>
+                            <img src={LogoBlue} alt="" className='logo-book absolute' />
+                            </div>
+
+                            {/* Spacer Div for Proper Positioning */}
                             <div></div>
-                            <div className='book-text book-group-hover'>Achievement</div>
+
+                            {/* Text Positioned at 3/4th */}
+                            <div className='book-text book-group-hover flex items-end justify-center'>
+                                Achievement
+                            </div>
                         </Link>
                     </div>
-                    <div className='px-[100px]'>
-                        <Link to='/about-info' className='relative book-button book-left grid grid-rows-2 book-hover group'>
-                            <div className='top-half-book'></div>
-                            <div className='absolute translate logo-book z-10'><img src={LogoBlue} alt="" /></div>
+                    <div className='book-space'>
+                        <Link to='/about-info' className='relative book-button book-left grid grid-rows-[3fr,1fr] book-hover group'>
+                            {/* Centered White Background */}
+                            <div className='top-half-book flex items-center justify-center'>
+                            <img src={LogoBlue} alt="" className='logo-book absolute' />
+                            </div>
+
+                            {/* Spacer Div for Proper Positioning */}
                             <div></div>
-                            <div className='book-text book-group-hover'>Skills</div>
+
+                            {/* Text Positioned at 3/4th */}
+                            <div className='book-text book-group-hover'>
+                                Skills
+                            </div>
                         </Link>
                     </div>
                 </div>
