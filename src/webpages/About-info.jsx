bb547840280e2c809ca0edfc6d7267b06147397a
Wafeechan <awn.1905@gmail.com>
2025-01-01 15:06:28 +0800

    Update About-info.jsx

--- src/webpages/About-info.jsx
+++ src/webpages/About-info.jsx
@@ -46,7 +46,7 @@ function AboutInfo() {
     ];
 
     return (
-        <div className="p-10 bg-blue-400 h-screen sm:h-[120vh] md:h-screen grid grid-rows-[1fr,4fr] gap-10 justify-center">
+        <div className="p-10 bg-blue-400 h-screen sm:h-[120vh] md:h-screen grid grid-rows-[1fr,4fr] gap-4 sm:gap-10 justify-center">
             <div className="text-white font-Inter text-3xl sm:text-5xl font-semibold flex mt-auto mx-auto">About</div>
             <div className="hidden md:block bg-white rounded-xl w-[1000px] h-[480px]">
                 <TabImageSlider images={images} width="200px" height="250px" titles={titles} 
@@ -54,13 +54,13 @@ function AboutInfo() {
             </div>
 
             {/* Tablet layout */}
-            <div className="hidden sm:block md:hidden bg-white rounded-xl w-[600px] h-[700px]">
+            <div className="hidden sm:block md:hidden bg-white rounded-xl w-[600px] h-[700px] mb-10">
                 <TabImageSlider images={images} width="200px" height="280px" titles={titles} 
                 details1={details1} details2={details2} details3={details3} descriptions={descriptions}/>
             </div>
 
             {/* Phone layout */}
-            <div className="block sm:hidden bg-white rounded-xl w-[300px] h-[450px]">
+            <div className="block sm:hidden bg-white rounded-xl w-[300px] h-[450px] mb-10">
                 <TabImageSlider images={images} width="100px" height="150px" titles={titles} 
                 details1={details1} details2={details2} details3={details3} descriptions={descriptions}/>
             </div>
