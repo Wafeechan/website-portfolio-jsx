306c0439eafb9f908958fcbce9df5d1545b01582
Wafeechan <awn.1905@gmail.com>
2025-01-01 14:54:47 +0800

    Added responsiveness
    Responsiveness for Tablet and Phone layout

--- src/components/tabImageSlider.jsx
+++ src/components/tabImageSlider.jsx
@@ -13,9 +13,9 @@ function TabImageSlider({ images, width, height, titles, details1, details2, det
 
   return (
     <div className="gap-4 w-full h-auto pt-10 px-4">
-        <div className='grid grid-cols-[1fr,1fr,2fr]'>
+        <div className='hidden md:grid grid-cols-[1fr,1fr,2fr]'>
             {/* Top Left Image */}
-            <div className="relative flex overflow-hidden m-auto" style={{width, height}}>
+            <div className="relative flex overflow-hidden mx-auto my-8" style={{width, height}}>
                 <img
                 src={images[currentIndex]}
                 alt={titles[currentIndex]}
@@ -39,7 +39,7 @@ function TabImageSlider({ images, width, height, titles, details1, details2, det
       
 
       {/* Bottom Navigation */}
-      <div className="col-span-2 flex items-center justify-center mt-[70px]">
+      <div className="hidden col-span-2 md:flex items-center justify-center mt-[70px]">
         <button
           onClick={handlePrev}
           className="px-2 py-1 bg-gray-300 hover:bg-gray-400 text-sm rounded-l-md"
@@ -66,6 +66,120 @@ function TabImageSlider({ images, width, height, titles, details1, details2, det
           ❯
         </button>
       </div>
+
+        {/* -----------------------------------------For tablet layout---------------------------- */}
+        <div className='hidden md:hidden sm:grid grid-rows-3'>
+        <div className='grid grid-cols-2'>
+            {/* Top Left Image */}
+            <div className="relative flex overflow-hidden mx-auto" style={{width, height}}>
+                <img
+                src={images[currentIndex]}
+                alt={titles[currentIndex]}
+                className="w-full h-full object-cover"
+                />
+            </div>
+
+            {/* Right Side Description */}
+            <div className="grid grid-rows-2 gap-4 text-blue-950">
+                <div className='text-start mt-auto font-Inter font-semibold text-5xl'>{titles[currentIndex]}</div>
+                <div className='grid grid-row-5 text-sm'>
+                    <div>Position: {details1[currentIndex]}</div>
+                    <div>Location: {details2[currentIndex]}</div>
+                    <div>Period: {details3[currentIndex]}</div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+        <div className="justify-start pb-auto pt-6 px-2 text-base font-medium">{descriptions[currentIndex]}</div>
+
+        {/* Bottom Navigation */}
+        <div className="flex items-start justify-center mt-8">
+          <button
+            onClick={handlePrev}
+            className="px-1 py-[1px] bg-gray-300 hover:bg-gray-400 text-sm rounded-l-md"
+          >
+            ❮
+          </button>
+
+          <div className="flex space-x-2 mx-3 my-2">
+            {images.map((_, index) => (
+              <button
+                key={index}
+                onClick={() => setCurrentIndex(index)}
+                className={`w-2 h-2 rounded-full ${
+                  index === currentIndex ? 'bg-blue-500' : 'bg-gray-400'
+                }`}
+              />
+            ))}
+          </div>
+
+          <button
+            onClick={handleNext}
+            className="px-1 py-[1px] bg-gray-300 hover:bg-gray-400 text-sm rounded-r-md"
+          >
+            ❯
+          </button>
+        </div>
+      </div>
+
+        {/* -----------------------------------------For phone layout---------------------------- */}
+      <div className='sm:hidden grid grid-rows-3'>
+        <div className='grid grid-cols-2'>
+            {/* Top Left Image */}
+            <div className="relative flex overflow-hidden m-auto" style={{width, height}}>
+                <img
+                src={images[currentIndex]}
+                alt={titles[currentIndex]}
+                className="w-full h-full object-cover"
+                />
+            </div>
+
+            {/* Right Side Description */}
+            <div className="grid grid-rows-2 gap-4 text-blue-950">
+                <div className='text-start mt-auto font-Inter font-semibold text-2xl'>{titles[currentIndex]}</div>
+                <div className='grid grid-row-5 text-[9px]'>
+                    <div>Position: {details1[currentIndex]}</div>
+                    <div>Location: {details2[currentIndex]}</div>
+                    <div>Period: {details3[currentIndex]}</div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+        <div className="justify-start pt-4 px-2 text-[8px] font-medium">{descriptions[currentIndex]}</div>
+
+        {/* Bottom Navigation */}
+        <div className="flex items-center justify-center">
+          <button
+            onClick={handlePrev}
+            className="px-1 py-[1px] bg-gray-300 hover:bg-gray-400 text-sm rounded-l-md"
+          >
+            ❮
+          </button>
+
+          <div className="flex space-x-2 mx-3">
+            {images.map((_, index) => (
+              <button
+                key={index}
+                onClick={() => setCurrentIndex(index)}
+                className={`w-2 h-2 rounded-full ${
+                  index === currentIndex ? 'bg-blue-500' : 'bg-gray-400'
+                }`}
+              />
+            ))}
+          </div>
+
+          <button
+            onClick={handleNext}
+            className="px-1 py-[1px] bg-gray-300 hover:bg-gray-400 text-sm rounded-r-md"
+          >
+            ❯
+          </button>
+        </div>
+      </div>
+
+      
     </div>
   );
 }
--- src/webpages/About-info.jsx
+++ src/webpages/About-info.jsx
@@ -34,22 +34,34 @@ function AboutInfo() {
     const descriptions = [
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
 
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada.',
+
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc.',
 
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
 
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
-
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
+    'congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
 
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
     ];
 
     return (
-        <div className="p-10 bg-blue-400 h-screen grid grid-rows-[1fr,4fr] gap-10 justify-center">
+        <div className="p-10 bg-blue-400 h-screen sm:h-[120vh] md:h-screen grid grid-rows-[1fr,4fr] gap-10 justify-center">
             <div className="text-white font-Inter text-5xl font-semibold flex mt-auto mx-auto">About</div>
-            <div className="bg-white rounded-xl w-[1000px] h-[450px]">
+            <div className="hidden md:block bg-white rounded-xl w-[1000px] h-[480px]">
                 <TabImageSlider images={images} width="200px" height="250px" titles={titles} 
+                details1={details1} details2={details2} details3={details3} descriptions={descriptions}/>
+            </div>
+
+            {/* Tablet layout */}
+            <div className="hidden sm:block md:hidden bg-white rounded-xl w-[600px] h-[700px]">
+                <TabImageSlider images={images} width="200px" height="280px" titles={titles} 
+                details1={details1} details2={details2} details3={details3} descriptions={descriptions}/>
+            </div>
+
+            {/* Phone layout */}
+            <div className="block sm:hidden bg-white rounded-xl w-[300px] h-[450px]">
+                <TabImageSlider images={images} width="100px" height="150px" titles={titles} 
                 details1={details1} details2={details2} details3={details3} descriptions={descriptions}/>
             </div>
 
