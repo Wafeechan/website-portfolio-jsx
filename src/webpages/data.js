235f536b9933bca428b40a36a2526db1c3bd53e8
Wafeechan <awn.1905@gmail.com>
2025-01-02 16:25:05 +0800

    Update data.js

--- src/webpages/data.js
+++ src/webpages/data.js
@@ -2,47 +2,121 @@ import Image1 from '/src/assets/image/placeholder1.jpg';
 import Image2 from '/src/assets/image/placeholder2.jpg';
 import Image3 from '/src/assets/image/placeholder3.png';
 
-export const images = [Image1, Image2, Image3, Image1, Image2, Image3];
+export const images = [Image1, Image2, Image3, Image1, Image2, Image3, Image1, Image2, Image3, Image1, Image2, Image3];
 
-export const titles = ['Osaka University', 'Portfolio 2', 'Portfolio 3', 'Portfolio 4', 'Portfolio 5', 'Portfolio 6'];
+export const titles = [
+    //Education
+    "Universiti Teknologi Brunei", 
+    "Maktab Duli PMAMB", 
+    "SM Sultan Hassan Bangar", 
 
+    //Experience
+    "Stoneville Hotel", 
+    "Osaka University", 
+    "Utama Grand Superstore",
+
+    //Achievement
+    "Most Progressive Student",
+
+    //Skills
+    "Programming",
+    "Graphic Design",
+    "Microsoft Software",
+    "Language",
+    "General Skills",
+];
+
+// Position
 export const details1 = [
-    'Research Assistant',
-    '',
-    '',
-    'Research Assistant',
-    '',
-    '',
+    //Education
+    "Undergraduate",
+    "Student",
+    "Student",
+
+    //Experience
+    "Waiter/Bartender",
+    "Research Assistant",
+    "Sales Assistant",
+
+    //Achievement
+    "UTB Student",
+
+    //Skills
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
 ]
 
+//Location
 export const details2 = [
-    'Japan',
-    '',
-    '',
-    'Japan',
-    '',
-    '',
+    //Education
+    "Brunei Darussalam",
+    "Gadong, Brunei Muara",
+    "Bangar, Temburong",
+
+    //Experience
+    "Temburong, Brunei",
+    "Osaka, Japan",
+    "Gadong, Brunei",
+
+    //Achievement
+    "-",
+
+    //Skills
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
 ]
 
+//Period
 export const details3 = [
-    'May 2023 - July 2023',
-    '',
-    '',
-    'May 2023 - July 2023',
-    '',
-    '',
+    //Education
+    "July 2020 - June 2024",
+    "2018 - 2019",
+    "2014 - 2017",
+
+    //Experience
+    "October 2024 - December 2024",
+    "May 2023 - July 2023",
+    "Jan 2020 - June 2020",
+
+    //Achievement
+    "2024",
+
+    //Skills
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
 ]
 
 export const descriptions = [
-'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
+    //Education
+    "Full-time bachelor's degree student under government scholarship. Relevant courseworks includes: Programming, Graphic Design & Artificial Intelligence. Awarded First Class Honours.",
 
-'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada.',
+    "A college located at Gadong, Brunei Muara, Brunei Darussalam. I attended there for 2 year's to get my A Level certification. I took 3 'A' Level subjects: Mathematics, Physics & Computer Science.",
 
-'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc.',
+    "A secondary school that I attend in my hometown city, Temburong, and is the only secondary school there until now. I attended there for 4 years and was awarded 10 'O' Levels.",
 
-'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
+    //Experience
+    "A retail position in which we are mostly responsible for customer services within the hotel’s restaurant such as taking and delivering orders, tending the cashier and acting as a barista. Within the first two weeks of me working, I was already given the responsibility of helping manage an event hosted by the company’s boss, occupied by more than 70 important figures, along with a buffet during lunchtime which had been occupied by almost 150 people, in which both ended up in a success.",
 
-'congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
+    "Chosen as one of a select group of top students to attend the Osaka University International Certificate Program (OUICP) as a research assistant, with a primary focus on the United Nations’ Sustainable Development Goals (SDGs). There we were given the opportunity to propose a presentation idea on how to integrate halal science into one of their state-of-the-art technologies which they had recently developed.",
 
-'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar congue pharetra. Proin quis tortor pellentesque, laoreet risus tempor, fermentum dolor. Fusce nec enim quam. Duis lorem nunc, varius nec consequat nec, ornare non quam. Integer eget metus sit amet mi lobortis facilisis at eu lectus. Ut varius enim non est aliquet auctor. Aenean facilisis risus in laoreet efficitur. Phasellus imperdiet non nisl nec sodales. Nunc eleifend mauris sed nisi congue interdum. Vivamus porttitor eros iaculis velit malesuada, eget consectetur augue rhoncus. Aenean convallis, ante eget pulvinar ornare, nulla nulla egestas nisl, bibendum mattis enim nunc ac ante. Curabitur aliquet auctor leo id accumsan.',
+    "A sales assistant helps customers, handles transactions, and maintains store displays to ensure a positive shopping experience. Here I have shown great discipline and commitment to the job no matter how tough the situation gets, especially during the COVID-19 era, along with being able to manage a grueling 3-week period of overtime for preparation of stock checking of the store.",
+
+    //Achievements
+    "An award given to students who achieved astounding progress over the course of studying in UTB, showing great improvement in results over the year. Here, I was awarded a 1-year professional membership sponsored by the British Computer Society(BCS).",
+
+    //Skills
+    "Unity, HTML, Arduino, Vite, ReactJS, TailwindCSS.",
+    "Adobe Photoshop, Illustrator, After Effects, Premiere Pro, Krita.",
+    "Microsoft Word, Powerpoint, Excel.",
+    "Malay & English.",
+    "Skilled in, Problem Solving, Analysis, Customer Services, Interpersonal Engagement.",
 ];
