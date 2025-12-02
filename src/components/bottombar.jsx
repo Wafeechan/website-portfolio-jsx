1075bbebe0d01f2b33c6c577ea0dcd9a1c40d97c
Wafeechan <awn.1905@gmail.com>
2025-12-02 11:48:39 +0800

    Update bottombar.jsx

--- src/components/bottombar.jsx
+++ src/components/bottombar.jsx
@@ -1,8 +1,8 @@
 function Bottombar(){
     return(
-        <div className='w-full text-gray-700 font-Inter-thin font-thin z-50'>
+        <div className='w-full text-gray-100 font-Inter-thin font-thin z-50'>
             <div className="m-0">
-                <nav className="fixed bottom-0 left-0 z-10 w-full h-[50px] sm:h-[70px] bg-white shadow-md">
+                <nav className="fixed bottom-0 left-0 z-10 w-full h-[50px] sm:h-[70px] bg-gray-800 shadow-md">
                     <div className="cotainer mx-auto sm:my-6 my-3 justify-between text-center">
                         <h1>
                             You can find more about me here on{' '}
