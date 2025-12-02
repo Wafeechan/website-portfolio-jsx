f303cea6a321d4df5f1f0ead5276ad3f37ab9ccf
Wafeechan <awn.1905@gmail.com>
2025-12-02 15:24:47 +0800

    Update Homev2.jsx

--- src/webpages/Homev2.jsx
+++ src/webpages/Homev2.jsx
@@ -1,10 +1,13 @@
-
+import FloatingItem from "/src/components/floatingItem.jsx";
+import Shiroko from "/assets/chibiShiroko.jpg";
 
 function Homev2(){
     return(
-        <>
-        
-        </>
+        <div>
+
+            {/* Floating object */}
+            <FloatingItem image={Shiroko} />
+        </div>
     )
 }
 
