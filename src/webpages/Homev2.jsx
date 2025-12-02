d7067189f2c650f2d38d01886c971a87c122eb32
Wafeechan <awn.1905@gmail.com>
2025-12-02 15:49:25 +0800

    Update Homev2.jsx
    Added hover interaction

--- src/webpages/Homev2.jsx
+++ src/webpages/Homev2.jsx
@@ -1,12 +1,18 @@
 import FloatingItem from "/src/components/floatingItem.jsx";
-import Shiroko from "/assets/chibiShiroko.jpg";
+import idleImage from "/assets/floatingItem/chibiShiroko.jpg";
+import hoverImage from "/assets/floatingItem/shirokoHover.png";
+import animFrame1 from "/assets/floatingItem/shirokoPickUp.jpg";
+import animFrame2 from "/assets/floatingItem/shirokoPickUp2.jpg";
 
 function Homev2(){
     return(
         <div>
-
-            {/* Floating object */}
-            <FloatingItem image={Shiroko} />
+            <FloatingItem
+                idleImage={idleImage}
+                hoverImage={hoverImage}
+                animFrame1={animFrame1}
+                animFrame2={animFrame2}
+            />
         </div>
     )
 }
